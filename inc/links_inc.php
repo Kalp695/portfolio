4bc096e842997ea007479457fdc60576033dfb08
Mika81 <webdeveloper@diw.fr>
2014-05-09 14:11:42 +0200

    modify links on secretPage

--- inc/links_inc.php
+++ inc/links_inc.php
@@ -4,14 +4,20 @@
     </div>
     <div class='contenuBlogDroite'>
         <ul>
+            <li><a href="javascript:void(window.open('https://www.google.com/analytics/web/?authuser=0#report/visitors-overview/a48682190w80260856p83024188/'))">Google analytics</a></li>
             <li><a href="javascript:void(window.open('http://www.responsinator.com/?url=www.diw.fr'))">Responsinator (Test Responsive)</a></li>
             <li><a href="javascript:void(window.open('http://devdocs.io/'))">Dev docs</a></li>
             <li><a href="javascript:void(window.open('https://www.iconfinder.com/'))">Icon Finder</a></li>
             <li><a href="javascript:void(window.open('http://www.git-tower.com/blog/git-cheat-sheet-detail/'))">Git Tower</a></li>
             <li><a href="javascript:void(window.open('http://rogerdudler.github.io/git-guide/index.fr.html'))">Git Hub Guide</a></li>
+            <li><a href="javascript:void(window.open('http://nvie.com/posts/a-successful-git-branching-model/'))">Git branching model</a></li>
             <li><a href="javascript:void(window.open('http://www.cheat-sheets.org/'))">Cheat Sheets</a></li>
             <li><a href="javascript:void(window.open('http://www.gethifi.com/tools/regex'))">Testeur expressions régulières</a></li>
             <li><a href="javascript:void(window.open('http://i.kinja-img.com/gawker-media/image/upload/drn5gedukwnkq04nufma.png'))">Commandes de base sous Linux</a></li>
+            <li><a href="javascript:void(window.open('http://www.colourlovers.com/'))">Palette et idées couleurs</a></li>
+            <li><a href="javascript:void(window.open('http://colorschemedesigner.com/'))">Bon assemblage à partir d’une couleur de base</a></li>
+            <li><a href="javascript:void(window.open('http://www.regexper.com/'))">REGEPEX - test javascript expressions</a></li>
+                    
         </ul>
     </div>
 </div>
